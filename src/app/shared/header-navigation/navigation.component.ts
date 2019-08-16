2f69a1c2fdc1f1505202d2310ba28a3091ba9aed
nicolasey <nicolas.sey@gmail.com>
2019-08-16 15:35:19 +0200

    add pj nav

--- src/app/shared/header-navigation/navigation.component.ts
+++ src/app/shared/header-navigation/navigation.component.ts
@@ -1,16 +1,37 @@
-import { Component, AfterViewInit, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 import { NgbModal, ModalDismissReasons, NgbPanelChangeEvent, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { Store, Select } from '@ngxs/store';
+import { Navigate } from '@ngxs/router-plugin';
+import * as _ from 'lodash';
+
+import { AuthState } from '../../@swor/auth/state/auth.state';
+import { Observable, Subscription } from 'rxjs';
+import { User } from '../../@swor/auth/models/user.model';
+import { PersonnageState } from '../../@swor/auth/state/personnage.state';
+import { Personnage } from '../../@swor/auth/models/personnage.model';
+import { Logout } from '../../@swor/auth/state/auth.actions';
+import { ChangeCurrentPersonnage } from '../../@swor/auth/state/personnage.actions';
+
 declare var $: any;
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html'
 })
-export class NavigationComponent implements AfterViewInit {
+export class NavigationComponent implements OnInit, OnDestroy {
   @Output() toggleSidebar = new EventEmitter<void>();
 
   public config: PerfectScrollbarConfigInterface = {};
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private store: Store) {}
+
+  @Select(AuthState.user) user$: Observable<User>;
+  @Select(PersonnageState.personnage) pj$: Observable<Personnage>;
+  private userSubscription: Subscription;
+  private pjSubscription: Subscription;
+  public user: User;
+  public inactivePersonnages: Personnage[];
+  public currentPersonnage: Personnage;
 
   public showSearch = false;
 
@@ -78,5 +99,42 @@ export class NavigationComponent implements AfterViewInit {
     }
   ];
 
-  ngAfterViewInit() {}
+  ngOnInit() {
+    this.userSubscription = this.user$.subscribe(
+      (user) => {
+        this.user = user;
+      },
+      (err) => console.log(err)
+    );
+
+    this.pjSubscription = this.pj$.subscribe(
+      (personnage) => {
+        this.currentPersonnage = personnage;
+        this.inactivePersonnages = _.filter(this.user.personnages, (item: Personnage) => item !== personnage);
+        console.log('current nav = ', personnage);
+      },
+      err => console.error(err)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+    this.pjSubscription.unsubscribe();
+  }
+
+  logout() {
+    this.store.dispatch(new Logout);
+  }
+
+  login() {
+    this.store.dispatch(new Navigate(['/auth/login']));
+  }
+
+  addPersonnage() {
+    this.store.dispatch(new Navigate(['personnage/creer']));
+  }
+
+  change(personnage: Personnage) {
+    this.store.dispatch(new ChangeCurrentPersonnage(personnage));
+  }
 }
