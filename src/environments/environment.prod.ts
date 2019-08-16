4dd9326c475ea530341e473af0d82e0bf4275a62
nicolasey <nicolas.sey@gmail.com>
2019-08-16 19:22:12 +0200

    fix missing prod env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  base: {
+    apiUrl: 'http://localhost:8000/api/',
+    socket: 'ws://localhost:3000/',
+  },
+  storage: ['auth.token', 'auth.user', 'personnage.personnage']
 };
