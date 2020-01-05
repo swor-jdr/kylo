8dcb512d1cd5ad8fd609c75bb16a87eea850e31b
Nicolas Seynaeve <nicolas.sey@gmail.com>
2020-01-05 19:34:02 +0100

    wip article view

--- src/app/@swor/holonews/views/article/article.component.ts
+++ src/app/@swor/holonews/views/article/article.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogService } from '../../services/blog.service';
+import { BlogPost } from '../../services/article.model';
 
 @Component({
   selector: 'app-article',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private blog$: BlogService) { }
 
-  constructor() { }
+  article: BlogPost;
 
   ngOnInit() {
+    this.route.params.subscribe(
+      params => this.loadArticle(params['slug']),
+      err => console.error(err)
+    );
   }
 
+  loadArticle(slug: string) {
+    this.blog$.getBySlug(slug).subscribe(
+      post => this.article = post,
+      err => console.error(err)
+    );
+  }
 }
