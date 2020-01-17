68a1229fae1bc801f61be923c63691096abbcc92
Nicolas Seynaeve <nicolas.sey@gmail.com>
2020-01-17 16:38:16 +0100

    add mixpanel state

--- src/app/@core/analytics/mixpanel.actions.ts
+++ src/app/@core/analytics/mixpanel.actions.ts
@@ -2,3 +2,8 @@ export class Track {
     static readonly type = '[Mixpanel] Track';
     constructor(public name: string, public action: any) {}
 }
+
+export class Init {
+    static readonly type = '[Mixpanel] Initialize';
+    constructor(public userToken: string) {}
+}
--- src/app/@core/analytics/mixpanel.state.ts
+++ src/app/@core/analytics/mixpanel.state.ts
@@ -1,9 +1,18 @@
-import { State, Action } from '@ngxs/store';
-import { Track } from './mixpanel.actions';
+import { State, Action, StateContext } from '@ngxs/store';
+import { Track, Init } from './mixpanel.actions';
+import * as mixpanel from 'mixpanel-browser';
+import { environment } from '../../../environments/environment';
 
 @State<{}>({ name: 'mixpanel', defaults: {} })
 export class MixpanelState {
-    @Action(Track)
-    track(ctx, event) {}
-}
+    @Action(Init)
+    init(ctx: StateContext<{}>, event: Init) {
+        mixpanel.init(environment.mixpanel.projectToken);
+        mixpanel.identify(event.userToken);
+    }
 
+    @Action(Track)
+    track(ctx: StateContext<{}>, event: Track) {
+        mixpanel.track(event.name, event.action);
+    }
+}
