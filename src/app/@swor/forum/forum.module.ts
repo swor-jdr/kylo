6c660cd92fdda40aaa786609a80999b7e1f43358
Nicolas Seynaeve <nicolas.sey@gmail.com>
2020-02-02 11:45:04 +0100

    add forum module structure

---
+++ src/app/@swor/forum/forum.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+  ]
+})
+export class ForumModule { }
