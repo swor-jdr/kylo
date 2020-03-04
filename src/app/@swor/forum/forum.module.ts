b5805ba324683d1f34900df59315040e5f0fdc69
Nicolas Seynaeve <nicolas.sey@gmail.com>
2020-03-04 13:27:06 +0100

    add services to forum module

--- src/app/@swor/forum/forum.module.ts
+++ src/app/@swor/forum/forum.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TopicService } from './services/topic.service';
+import { ForumService } from './services/forum.service';
+import { PostService } from './services/post.service';
 
 @NgModule({
   declarations: [],
@@ -10,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     FormsModule,
     ReactiveFormsModule,
     NgbModule,
-  ]
+  ],
+  providers: [TopicService, ForumService, PostService]
 })
 export class ForumModule { }
